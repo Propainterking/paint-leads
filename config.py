05c93805ee290f560bbf4aa45b9fa0260435be6a
Propainterking <Coxscoatings@gmail.com>
2025-07-09 00:16:29 -0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+LOCATIONS = [
+    "Conway SC", "Myrtle Beach SC", "North Myrtle Beach SC",
+    "Whiteville NC", "Tabor City NC", "Chadbourn NC"
+]
+
+KEYWORDS = [
+    "interior painter", "exterior painter", "need a painter", "painting job",
+    "house painting", "cheap painter", "licensed painter",
+    "paint my house", "residential painting", "professional painter",
+    "pressure wash and paint", "deck painting", "repaint living room",
+    "color change", "trim touch-up"
+]
